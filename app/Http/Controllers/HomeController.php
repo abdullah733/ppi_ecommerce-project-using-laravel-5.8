8cc91c8e3e44489a2d0d712ede5ade49990bd571
abdullah <abdulla733824@gmail.com>
2019-05-21 22:49:17 +0600

    css, js file generated using mix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller
 {
     public function showHome()
     {
-        return view('welcome');
+        return view('home');
    }
     public function showAbout()
     {
--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -12,4 +12,5 @@ class OrderProduct extends Model
      * @var array
      */
     protected $guarded = [];
+
 }
